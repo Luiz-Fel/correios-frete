e20cb8a0e8030f5cea696973bb635081cfb7c385
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-01-09 18:58:10 -0300

    setting up commit

---
+++ src/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './main.css';
+
+export default function Main() {
+
+    
+
+
+
+  return (
+    <>
+        <div>
+            <input type="text" id='' />
+        </div>
+        <div>
+            <p>Frete: {'null'}</p>
+        </div>
+    </>
+  )
+}
