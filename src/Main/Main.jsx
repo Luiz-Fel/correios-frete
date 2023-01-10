b8e64eccf7e41c88fd98e1cb17386424957ab959
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2023-01-09 21:18:15 -0300

    Added cep check

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -3,17 +3,39 @@ import './main.css';
 
 export default function Main() {
 
-    
+const [frete, setFrete] = React.useState(null);
 
+
+const submitHandler =   (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+     fetch(`http://localhost:3001/frete/?tracking=${data.tracking}`)
+    .then(response => response.json())
+    .then(data => setFrete(data))
+    .catch(err => console.log(err))
+
+    console.log(frete);
+}
 
 
   return (
     <>
+        <form className='form-group' onSubmit={submitHandler}>
+            <input type="text" 
+            name='tracking' 
+            className='form-control'/>
+            <button className='btn btn-primary' 
+            type='submit'
+            >Buscar</button>
+        </form>
         <div>
-            <input type="text" id='' />
-        </div>
-        <div>
-            <p>Frete: {'null'}</p>
+          {
+            frete != null ? <p>Frete: {frete.logradouro}</p> : null
+          }
         </div>
     </>
   )
